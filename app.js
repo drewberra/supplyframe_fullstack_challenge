06184ed1d8c7f73c18fa3087203816c374a78bd3
Drew Berra <drew.berra@gmail.com>
2021-07-29 17:01:34 -0700

    Rendering server side with ejs.

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const http = require("http");
 const ejs = require("ejs");
 
+require("dotenv").config();
+
 const port = process.env.PORT;
 
 const server = http.createServer((request, response) => {
-    console.log("Nice request bro");
+    let test = [{name: "project 1"}, {name: "project 2"}, {name: "project 3"}];
+    ejs.renderFile("templates/index.ejs", {projects: test}, {}, function (err, template) {
+        if (err) {
+            throw err;
+        }
+        else {
+            response.setHeader('Content-Type', "text/html");
+            response.end(template);
+        }
+    });
 });
 
 server.listen(port, () => {
