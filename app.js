a50b281428dceb39917a755ae5880832cb431030
drewnetconsensus <drew@netconsensusinc.com>
2021-07-31 16:04:55 -0700

    Created the project_page.ejs that is currently displaying the basic information that we need for that page.

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const request = require("request");
 
-
 require("dotenv").config();
 
 // Get the port that the server is going to run on from the environment variables
 const port = process.env.PORT;
 
-/************************************************************************************************************************
+/********************************************************************************************************************
 * Some functions based on example from: https://github.com/Supplyframe/had-api-example/blob/master/nodejs/server.js *
-************************************************************************************************************************/
+********************************************************************************************************************/
 
 function parseJSON (value) {
     let parsed;
@@ -22,105 +21,170 @@ function parseJSON (value) {
 }
 
 // Initialize the object that contains the info to build the api so that we can make requests
-let apiData = {
-    // clientId: process.env.OAUTH_CLIENT_ID,
-    // clientSecret: process.env.OAUTH_CLIENT_SECRET,
-    // apiKey: process.env.OAUTH_API_KEY
-}
-
+let apiData = {}
 apiData.apiKey = process.env.OAUTH_API_KEY;
-// apiData.apiKey = '?api_key=' + apiData.userKey;
 apiData.apiUrl = 'https://api.hackaday.io/v1';
-// apiData.apiAuthUrl = 'https://api.hackaday.io/v1/me' + apiData.apiKey;
-// apiData.oAuthRedirect = 'https://hackaday.io/authorize?client_id=' + apiData.clientId + '&response_type=code';
-// apiData.createTokenUrl = function (code) {
-//     return ('https://auth.hackaday.io/access_token?' +
-//     'client_id=' + this.clientId +
-//     '&client_secret=' + this.clientSecret +
-//     '&code=' + code +
-//     '&grant_type=authorization_code');
-// };
-
-// apiData.oAuthToken = "";
 
 let app = express();
 app.set("views", __dirname + "/templates");
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-    // Check if we oAuthToken, if not redirect the response so that we can receive one
-    // if (!apiData.oAuthToken) {
-    //     res.redirect(apiData.oAuthRedirect);
-    //     return;
-    // }
+// Hashmap of user info by their id that we are using for localstorage
+// In a production setting we would want to store this in a database of some sort and make sure that we are updating the user information frequently
+let users = {};
 
+app.get("/", (req, res) => {
     let page = 1;
     if (req.query.page) {
         page = parseInt(req.query.page);
     }
 
-    request.get(apiData.apiUrl + `/projects?api_key=${apiData.apiKey}&page=${page}`, function (err, res3, body) {
+    request.get(apiData.apiUrl + `/projects?api_key=${apiData.apiKey}&page=${page}`, function (err, res2, body) {
         let bodyData = parseJSON(body);
         if (!bodyData) {
             console.log('\nError parsing bodyData');
             return;
         }
 
-        let pageInfo = {
-            projects: bodyData.projects,
-            page: page,
-        };
+        let projects = bodyData.projects;
 
-        console.log(req.headers.return_projects_only);
+        let userIdQueryString = "";
+        let userIdsToGet = new Set();
+        for (let i=0; i < projects.length; ++i) {
+            let userId = projects[i].owner_id;
+            if (!(userId in users) && !userIdsToGet.has(userId)) {
+                userIdsToGet.add(userId);
+                userIdQueryString += `${userId},`;
+            }
+        }
 
-        if (req.headers.return_projects_only !== undefined) {
-            res.render("components/projects.ejs", {
-                projects: bodyData.projects
+        if (userIdsToGet.size > 0) {
+            let userInfoUrl = `${apiData.apiUrl}/users/batch?ids=${userIdQueryString.slice(0, -1)}&api_key=${apiData.apiKey}`
+            request.get(userInfoUrl, function (err, res3, body) {
+                let userResBody = parseJSON(body);
+                if (!userResBody) {
+                    console.log('\nError parsing bodyData');
+                    return;
+                }
+
+                for (let i=0; i < userResBody.users.length; ++i) {
+                    users[userResBody.users[i].id] = userResBody.users[i];
+                }
+
+                for (let i=0; i < projects.length; ++i) {
+                    let project = projects[i];
+                    project.owner_meta_data = users[project.owner_id];
+                    projects[i] = project;
+                }
+
+                if (req.headers.return_projects_only !== undefined) {
+                    res.render("components/projects.ejs", {
+                        projects: projects
+                    });
+                }
+                else {
+                    res.render("index.ejs", {
+                        projects: projects,
+                        page: page
+                    });
+                }
             });
         }
         else {
-            res.render("index.ejs", {
-                projects: bodyData.projects,
-                page: page,
-            });
+            for (let i=0; i < projects.length; ++i) {
+                let project = projects[i];
+                project.owner_meta_data = users[project.owner_id];
+                projects[i] = project;
+            }
+
+            if (req.headers.return_projects_only !== undefined) {
+                res.render("components/projects.ejs", {
+                    projects: projects
+                });
+            }
+            else {
+                res.render("index.ejs", {
+                    projects: projects,
+                    page: page
+                });
+            }
         }
     });
 });
-//
-// app.get("/oauth_callback", (req, res) => {
-//     console.log(1);
-//     let code = req.query.code;
-//     if (!code) {
-//         return res.redirect('/');
-//     }
-//
-//     console.log(2);
-//     console.log('\nAccess code: ', code);
-//
-//     let postUrl = apiData.createTokenUrl(code);
-//
-//     console.log('\nPost Url: ', postUrl);
-//
-//     console.log(3);
-//     request.post(postUrl, function (err, res2, body) {
-//         let parsedData = parseJSON(body), token = null;
-//
-//         if (parsedData) {
-//             token = parsedData.access_token;
-//         }
-//
-//         if (!token) {
-//             console.log('\nError parsing access_token: ', body);
-//             return;
-//             // return res.redirect('/');
-//         }
-//
-//         console.log('\nToken: ', token);
-//         apiData.oAuthToken = token;
-//
-//         res.redirect("/");
-//     });
-// });
+
+app.get("/project/:id", async (req, res) => {
+    let projectId = req.params.id;
+
+    request.get(`${apiData.apiUrl}/projects/${projectId}?api_key=${apiData.apiKey}`, async function (err, res2, body) {
+        let projectInfo = parseJSON(body);
+        if (!projectInfo) {
+            console.log('\nError parsing bodyData');
+            return;
+        }
+
+        console.log(`Tags: ${projectInfo.tags}`);
+
+        let relatedProjects = [];
+        let promiseArray = [];
+        for (let i=0; i < projectInfo.tags.length; ++i) {
+            promiseArray.push(
+                searchForProjectsByTag(
+                    projectInfo.tags[i],
+                    function (projectsWithMatchingTag) {
+                        relatedProjects = relatedProjects.concat(projectsWithMatchingTag);
+                    }
+                )
+            );
+        }
+
+        Promise.all(promiseArray).then(function () {
+            let relatedProjectsHash = {};
+            for (let i=0; i < relatedProjects.length; ++i) {
+                if (!(relatedProjects[i].id in relatedProjectsHash)) {
+                    relatedProjectsHash[relatedProjects[i].id] = relatedProjects[i];
+                }
+            }
+
+            projectInfo.relatedProjects = Object.values(relatedProjectsHash);
+
+            res.render("project_page.ejs", {
+                project: projectInfo
+            });
+        })
+    });
+});
+
+// function that searches for projects by tagName
+// arguments - tagName:str, callback:func
+// returns any matching projects found via the callback
+function searchForProjectsByTag(tagName, callback) {
+    return new Promise(((resolve, reject) => {
+        request.get(`${apiData.apiUrl}/projects/search?search_term=${tagName}&api_key=${apiData.apiKey}`, function (err, res, body) {
+            if (err) {
+                reject(err);
+            }
+
+            let searchResults = parseJSON(body);
+            if (!searchResults) {
+                console.log('\nError parsing bodyData');
+                reject("Error parsing bodyData");
+                return;
+            }
+
+            let projects = searchResults.projects;
+
+            let relatedProjects = [];
+            for (let i=0; i < projects.length; ++i) {
+                if(projects[i].tags && projects[i].tags.includes(tagName)) {
+                    relatedProjects.push(projects[i]);
+                }
+            }
+
+            callback(relatedProjects);
+            resolve(body);
+        });
+    }))
+}
 
 app.listen(port, () => {
     console.log(`listening at: http://localhost:${port}/`);
